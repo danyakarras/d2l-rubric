af2a1dd9c8c353c04cf2a25f1623755ad1268f1c
Dave Westerveld <Dave.Westerveld@D2L.com>
2018-06-08 08:39:54 -0400

    add initial test
    (cleanup needed)

--- test/components/d2l-rubric.js
+++ test/components/d2l-rubric.js
@@ -1,10 +1,16 @@
 /* global suite, test, fixture, expect, suiteSetup, suiteTeardown, sinon */
 
 'use strict';
-
+	
 suite('<d2l-rubric>', function() {
 
 	var element, sandbox;
+	function myAsyncFunction(callback) {
+		// 500ms delay before callback
+		setTimeout(function() {
+		  callback(element);
+		}, 500);
+	  }
 
 	suiteSetup(function() {
 		sandbox = sinon.sandbox.create();
@@ -19,6 +25,26 @@ suite('<d2l-rubric>', function() {
 
 		test('can be instantiated', function() {
 			expect(element.is).to.equal('d2l-rubric');
+		});
+
+		test('should do something that creates some UI', function (done) {
+			// var fixtureScope = document.querySelector('some selector');
+	
+			// do stuff with the fixture
+			myAsyncFunction(function(element){
+				attest.init('wcag2aa', function () {
+					// done();
+				})
+				var my_context;
+				my_context = Polymer.dom(element.root)
+				
+				attest.run(my_context,function (err, results) {
+					console.log(results.violations[0].tags);
+					expect(results.violations).to.equal('hi');
+					
+				});
+				done();
+			});
 		});
 
 	});
