ea6c1c1c6fe7a399fa65ea78afcf9bec1c71170d
Dave Westerveld <Dave.Westerveld@D2L.com>
2018-06-26 12:39:54 -0400

    update to use the attest-virtual repo

--- test/components/ally-test-script.js
+++ test/components/ally-test-script.js
@@ -1,25 +1,26 @@
 test_data = function(){
-            //disable a few rules that don't apply to our component
-            var rules_options = {
-                "rules":{
-                    'html-has-lang':{enabled:false},
-                    'landmark-one-main':{enabled:false},
-                    'page-has-heading-one':{enabled:false},
-                    'region':{enabled:false}
+    
+    //disable a few rules that don't apply to our component
+    var rules_options = {
+        "rules":{
+            'html-has-lang':{enabled:false},
+            'landmark-one-main':{enabled:false},
+            'page-has-heading-one':{enabled:false},
+            'region':{enabled:false}
+        }
+    }
+    let my_res = '_UNSET';
+    return attest.run(rules_options).then(
+        function(results){
+            my_res = results;
+            expect(results.violations.length).to.equal(0);
+        })
+        .catch(
+            function (err){
+                var i;
+                for (i=0; i < my_res.violations.length; i++) {
+                    console.log(JSON.stringify(my_res.violations[i]));
                 }
+                throw err
             }
-            let my_res = '_UNSET';
-            return attest.run(rules_options).then(
-                function(results){
-                    my_res = results;
-                    expect(results.violations.length).to.equal(0);
-                })
-                .catch(
-                    function (err){
-                        var i;
-                        for (i=0; i < my_res.violations.length; i++) {
-                            console.log(JSON.stringify(my_res.violations[i]));
-                        }
-                        throw err
-                    }
-                )};
+        )};
