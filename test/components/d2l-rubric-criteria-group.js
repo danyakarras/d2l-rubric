0540a4287c9cd2ac4a802d33942ac576397e8f80
Dave Westerveld <Dave.Westerveld@D2L.com>
2018-06-28 14:28:37 -0400

    trying to get element to fully render

--- test/components/d2l-rubric-criteria-group.js
+++ test/components/d2l-rubric-criteria-group.js
@@ -24,8 +24,35 @@ suite('<d2l-rubric-criteria-group>', function() {
 	});
 
 	suite ('Ally Test',function(){
-        suiteSetup(function() {
-            element = fixture('basic-criteria');
+        suiteSetup(function(done) {
+			
+			element = fixture('basic-criteria');
+			flush(function(){
+				done();
+			});
+			console.log(element.querySelector('d2l-tr').class)
+			// 	function waitForLoad(e) {
+					
+			// 		// if ( Polymer.dom(element.root).querySelectorAll('.criteria').length !== 0) {
+			// 		// 	console.log(Polymer.dom(element.root).querySelectorAll('.criteria'))
+			// 		if (e.detail.entity.getLinkByRel('self').href === 'data/rubrics/organizations/text-only/199/groups.json') {
+			// 			element.removeEventListener('d2l-rubric-entity-changed', waitForLoad);
+						
+			// 		}
+			// 	}
+			// 	stubWhitelist();
+			// 	element.addEventListener('d2l-rubric-entity-changed', waitForLoad);
+			// 	// element.href = 'data/rubrics/organizations/text-only/199/groups.json';
+			// 	element.token = 'foozleberries';
+				
+
+			// 	var my_rows = Polymer.dom(element.root).querySelectorAll('.criteria');
+			// 	for (var i =0; i < my_rows.length; i++){
+			// 		console.log(my_rows[i])
+			// 	}
+			// 	done();
+			// });
+			
         });
 
         test('d2l-rubric checks',test_data);
